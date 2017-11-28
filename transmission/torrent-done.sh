7e36f3453bd101045288cd3a3d66361a4765f2a6
Luis Augusto <lpenap@gmail.com>
2017-11-28 12:34:15 -0400

    Fix character missing in internal doc

--- transmission/torrent-done.sh
+++ transmission/torrent-done.sh
@@ -4,7 +4,7 @@
 #
 # The environment variable TO_EMAIL must be set in your prefs file:
 # If you are using the provided install script, you can type:
-# echo 'export TO_EMAIL="your@email.com' > ~/bin/torrent-done.prefs
+# echo 'export TO_EMAIL="your@email.com"' > ~/bin/torrent-done.prefs
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/torrent-done.prefs"
