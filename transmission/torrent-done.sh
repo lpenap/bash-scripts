58a11370c0d03d52d87063271a2a0080b2e7488c
Luis Augusto <lpenap@gmail.com>
2017-11-28 12:40:16 -0400

    Move mail command to variable for easy changing

--- transmission/torrent-done.sh
+++ transmission/torrent-done.sh
@@ -6,7 +6,9 @@
 # If you are using the provided install script, you can type:
 # echo 'export TO_EMAIL="your@email.com"' > ~/bin/torrent-done.prefs
 
+MAIL="mail"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/torrent-done.prefs"
 
-echo "Torrent done: $TR_TORRENT_NAME" | mail -s "[torrent-done] $TR_TORRENT_NAME" $TO_EMAIL
+echo "Torrent done: $TR_TORRENT_NAME" | $MAIL -s "[torrent-done] $TR_TORRENT_NAME" $TO_EMAIL
